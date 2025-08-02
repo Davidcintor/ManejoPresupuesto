8739cf51bab8c6cfee19f12147d11c52e1f9cc86
David Ivan� Ramirez <David_Ramirez23@unikino.edu.mx>
2025-08-02 11:12:16 -0700

    Add project files.

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ManejoPresupuesto.Validaciones;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+        public int UsuarioId { get; set; }
+        public int Orden { get; set; }
+    }
+}
